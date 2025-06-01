cd3df5fcd5b38dc720a0b1cade5cec40b4df2313
seega Macbook <jaimecegarra2005@gmail.com>
2025-06-01 14:26:57 +0200

    lexico casi completo

--- lexico.h
+++ lexico.h
@@ -1,41 +1,20 @@
-#define             VAR             1
-#define             CONST           2
-#define             INT             3
-#define             IF              4
-#define             ELSE            5
-#define             WHILE           6
-#define             PRINTF          7
-#define             IN          8
-#define             SEMICOLON       9
+#define             ENTERO          1
+#define             CADENA          2
+#define             ID              3
+#define             VAR             4
+#define             IF              5
+#define             ELSE            6
+#define             WHILE           7
+#define             PRINTF          8
+#define             DOTCOMMA        9
 #define             COMMA           10
-#define             ASSIGNOP        11
-#define             PLUSOP          12
-#define             MINUSOP         13
-#define             REALLIT         14
-
-
-
-/*
-//              PALABRAS RESERVADAS
-var                                         return VAR;
-const                                       return CONST;
-int                                         return INT;
-if                                          return IF;
-else                                        return ELSE;
-while                                       return WHILE;
-printf                                      return PRINTF;
-{entero}                                    return INTLITERAL;
-//              CARACTERES ESPECIALES
-";"                                         return DOTCOMMA;
-","                                         return COMMA;
-"+"                                         return PLUSOP;
-"-"                                         return MINUSOP;
-"*"                                         return MULTOP;
-"/"                                         return DIVOP;
-"("                                         return LPAREN;
-")"                                         return RPAREN;
-"{"                                         return LBRACE;
-"}"                                         return RBRACE;
-"?"                                         return QMARK;
-":"                                         return COLON;
-*/
+#define             PLUSOP          11
+#define             MINUSOP         12
+#define             MULTOP          13
+#define             DIVOP           14
+#define             LPAREN          15
+#define             RPAREN          16
+#define             LBRACE          17
+#define             RBRACE          18
+#define             QMARK           19
+#define             COLON           20
