2bed203d244d18d05b180c9be562bf030d07be7b
Jaime Cegarra <jaimecegarra2005@gmail.com>
2025-06-01 11:19:56 +0200

    definicion de reservadas y caracteres especiales

---
+++ lexico.h
@@ -0,0 +1,41 @@
+#define             VAR             1
+#define             CONST           2
+#define             INT             3
+#define             IF              4
+#define             ELSE            5
+#define             WHILE           6
+#define             PRINTF          7
+#define             IN          8
+#define             SEMICOLON       9
+#define             COMMA           10
+#define             ASSIGNOP        11
+#define             PLUSOP          12
+#define             MINUSOP         13
+#define             REALLIT         14
+
+
+
+/*
+//              PALABRAS RESERVADAS
+var                                         return VAR;
+const                                       return CONST;
+int                                         return INT;
+if                                          return IF;
+else                                        return ELSE;
+while                                       return WHILE;
+printf                                      return PRINTF;
+{entero}                                    return INTLITERAL;
+//              CARACTERES ESPECIALES
+";"                                         return DOTCOMMA;
+","                                         return COMMA;
+"+"                                         return PLUSOP;
+"-"                                         return MINUSOP;
+"*"                                         return MULTOP;
+"/"                                         return DIVOP;
+"("                                         return LPAREN;
+")"                                         return RPAREN;
+"{"                                         return LBRACE;
+"}"                                         return RBRACE;
+"?"                                         return QMARK;
+":"                                         return COLON;
+*/
